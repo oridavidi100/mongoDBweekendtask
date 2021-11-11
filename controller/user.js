3af2105601f72bb88799c9536c25bf518f26f35a
ori davidi <oridavidi100@gmail.com>
2021-11-11 16:33:51 +0200

    delete router

--- controller/user.js
+++ controller/user.js
@@ -90,18 +90,85 @@ exports.findByStartOfPhoneNum= async (req, res, next) => {
 };
 
 exports.updateCourse= async (req, res, next) => {
+    const Name=req.params.name
+    const newCourse=req.body.course
+    console.log(Name ,newCourse)
+
     try {
         // console.log("userlist");
-        Accountant.updateOne(({license_id:id},{city:newCity})).then(()=>{
-            res.send("update succesed")
-         })
+        await Students.updateOne({name:Name},{$push:{courses:newCourse}})
+        res.send("update succes")
         // this is what i assume you meant
-        if (studentlist.length === 0) next(new Error("no users found in db"));
     } catch (error) {
         next(error);
     }
 };
 
+
+exports.updateBirth= async (req, res, next) => {
+    const Name=req.params.name
+    const date=req.body.date
+    console.log(Name ,date)
+
+    try {
+        // console.log("userlist");
+        await Students.updateOne({name:Name},{birth:date})
+        res.send("update succes")
+        // this is what i assume you meant
+    } catch (error) {
+        next(error);
+    }
+};
+exports.listAllStudentsNameByLetter = async (req, res, next) => {
+    try {
+      const { letter } = req.params;
+      const updateStudentCourse = await Students.find({ name: { $regex: letter } });
+      console.log(updateStudentCourse, 'listAllStudentsNameByLetter');
+      res.status(200).json(updateStudentCourse);
+  
+      // this is what i assume you meant
+      if (updateStudentCourse.length === 0) next(new Error('no students found in db'));
+    } catch (error) {
+      next(error);
+    }
+  };
+  exports.listAllStudentssurNameByTLetter = async (req, res, next) => {
+    try {
+      const { letter1,letter2 } = req.params;
+      const updateStudentCourse = await Students.find({$or: [{ surame: { $regex: letter1 } }, { surName: { $regex: letter2 } }]} );
+      console.log(updateStudentCourse,);
+      res.status(200).json(updateStudentCourse);
+  
+      // this is what i assume you meant
+      if (updateStudentCourse.length === 0) next(new Error('no students found in db'));
+    } catch (error) {
+      next(error);
+    }
+  };
+  exports.deleteByName= async (req, res, next) => {
+    const Name=req.params.name
+
+    try {
+        // console.log("userlist");
+        await Students.remove({name:Name})
+        res.send("delete succes")
+        // this is what i assume you meant
+    } catch (error) {
+        next(error);
+    }
+};
+exports.deleteByBirth= async (req, res, next) => {
+    const date=req.params.date
+    console.log(date);
+    try {
+        // console.log("userlist");
+        const deletedStudent=await Students.findOneAndRemove({birth:date})
+        res.send(deletedStudent)
+        // this is what i assume you meant
+    } catch (error) {
+        next(error);
+    }
+};
 
 
 
--- routers/user.js
+++ routers/user.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { listAllUsers, addNewStudents,findByName ,findBycourse
-    ,findBycourseAndGender,findByDate,findByStartOfPhoneNum} = require("../controller/user");
+    ,findBycourseAndGender,findByDate,findByStartOfPhoneNum,updateCourse
+    ,updateBirth,listAllStudentsNameByLetter,listAllStudentssurNameByTLetter,
+    deleteByName,deleteByBirth} = require("../controller/user");
 // middleware that is specific to this router
 // router.use(function timeLog(req, res, next) {
 //     console.log("Time: ", Date.now());
@@ -9,7 +11,7 @@ const { listAllUsers, addNewStudents,findByName ,findBycourse
 // });
 
 
-// define the home page route
+//get
 router.get("/list/all", listAllUsers);
 router.post("/new", addNewStudents);
 router.get("/list/name/:name",findByName)
@@ -17,6 +19,15 @@ router.get("/list/course/:course",findBycourse)
 router.get("/list/course/:course/:gender",findBycourseAndGender)
 router.get("/list/younger",findByDate)
 router.get("/list/number/:number",findByStartOfPhoneNum)
-//
+
+//put
+router.put("/update/course/:name",updateCourse)
+router.put("/update/birth/:name",updateBirth)
+//search
+router.get("/search/name/:letter",listAllStudentsNameByLetter)
+router.get("/search/surName/:letter1,letter2",listAllStudentssurNameByTLetter)
+//delete
+router.delete("/delete/name/:name",deleteByName)
+router.delete("/delete/birth/:date",deleteByBirth)
 
 module.exports = router;
